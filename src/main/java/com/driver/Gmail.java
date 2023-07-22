2c17388812fad1dde75123c715d0e0bf0a22b71e
Ashwini Siddanagond Biradar <122633022+Ashwinibiradar33@users.noreply.github.com>
2023-07-22 19:54:25 +0530

    Update Gmail.java

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -97,4 +97,28 @@ trash.clear();
         // Return the maximum number of mails that can be stored in the inbox
          return inboxCapacity;
     }
+    private class Mail {
+        private Date date;
+        private String sender;
+        private String message;
+
+        public Mail(Date date, String sender, String message) {
+            this.date = date;
+            this.sender = sender;
+            this.message = message;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public String getSender() {
+            return sender;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 }
+
