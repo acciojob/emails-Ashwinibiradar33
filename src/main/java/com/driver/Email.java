cd18987caa62a072dd39f865941f0368887b354d
Ashwini Siddanagond Biradar <122633022+Ashwinibiradar33@users.noreply.github.com>
2023-07-22 21:28:46 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,28 +1,96 @@
 package com.driver;
-class Email {
+
+public class Email {
+
     private String emailId;
     private String password;
+    public Email()
+    {
 
-    public Email(String emailId) {
-        this.emailId = emailId;
-        this.password = "Accio@123"; // Default password
     }
 
-    public boolean changePassword(String oldPassword, String newPassword) {
-        if (!oldPassword.equals(this.password)) {
-            return false;
-        }
+    public Email(String emailId){
+        this.emailId = emailId;
+        this.password = "Accio@123";
+    }
 
-        // Check if newPassword meets all conditions
-        if (newPassword.length() >= 8 &&
-                newPassword.matches(".*[A-Z].*") &&
-                newPassword.matches(".*[a-z].*") &&
-                newPassword.matches(".*\\d.*") &&
-                newPassword.matches(".*[^a-zA-Z\\d].*")) {
-            this.password = newPassword;
-            return true;
-        }
+    public String getEmailId() {
+        return emailId;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean checkuppercase(String str)
+    {
+        for(int i=0;i<str.length();i++)
+        {
+            char ch = str.charAt(i);
+            if(ch>='A' && ch<='Z')
+            {
+                return true;
+            }
+        }
         return false;
+    }
+    public boolean checklowercase(String str)
+    {
+        for(int i=0;i<str.length();i++)
+        {
+            char ch = str.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean checkdigit(String str)
+    {
+        for(int i=0;i<str.length();i++)
+        {
+            char ch = str.charAt(i);
+            if(ch>='1' && ch<='9')
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean checkspecial(String str)
+    {
+        for(int i=0;i<str.length();i++)
+        {
+            char ch = str.charAt(i);
+            if(ch<='A' || ch>='B')
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isPasswordValid(String newPassword) {
+        // Password conditions: At least 8 characters, 1 uppercase, 1 lowercase, 1 digit, and 1 special character.
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        return newPassword.matches(regex);
+    }
+
+    public void changePassword(String oldPassword, String newPassword){
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+
+        if(password.equals(oldPassword)){
+            if(newPassword.length()>=8 && isPasswordValid(newPassword))
+            {
+                this.password = newPassword;
+            }
+
+        }
     }
 }
