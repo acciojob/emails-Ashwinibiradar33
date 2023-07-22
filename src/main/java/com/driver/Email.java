367bfc6d5e9329a3e57515490838fa506518add8
Ashwini Siddanagond Biradar <122633022+Ashwinibiradar33@users.noreply.github.com>
2023-07-22 21:04:27 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,27 +1,14 @@
 package com.driver;
+class Email {
+    private String emailId;
+    private String password;
 
-public class Email {
-      String emailId;
-     String password;
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
-        this.password = "Accio@123";
+        this.password = "Accio@123"; // Default password
     }
 
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public String getPassword() {
-        return password;
-    }    
-    public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+    public boolean changePassword(String oldPassword, String newPassword) {
         if (!oldPassword.equals(this.password)) {
             return false;
         }
