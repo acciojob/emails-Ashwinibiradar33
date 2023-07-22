01e7ac76d01b0dddf8a87c300dbc87d4912a1431
Ashwini Siddanagond Biradar <122633022+Ashwinibiradar33@users.noreply.github.com>
2023-07-22 21:09:05 +0530

    Update Meeting.java

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -4,8 +4,8 @@ package com.driver;
 import java.time.LocalTime;
 
 public class Meeting {
-    public LocalTime startTime;
-    public LocalTime endTime;
+     LocalTime startTime;
+     LocalTime endTime;
 
     public Meeting(LocalTime startTime, LocalTime endTime){
         this.startTime = startTime;
